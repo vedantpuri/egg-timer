ba3b502871034e28744cb64323616344132a0844
Vedant Puri <vedantpuri13@gmail.com>
2018-02-17 14:18:35 -0500

    Initial Commit

---
+++ eggTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  eggTimer
+//
+//  Created by Vedant Puri on 2/17/18.
+//  Copyright © 2018 Vedant Puri. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
